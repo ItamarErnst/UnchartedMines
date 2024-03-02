0ddb2f9f40ba60e1d45305adb7aac39497bb08ff
Itamar\Erlich <Itamar@lonebot.net>
2024-03-02 16:06:42 +0200

    Connect resource and buttons

--- UnchartedMines/Assets/Scripts/ResourceController.cs
+++ UnchartedMines/Assets/Scripts/ResourceController.cs
@@ -1,12 +1,20 @@
-﻿using UnityEngine;
+﻿using System;
+using TMPro;
+using UnityEngine;
 
 public class ResourceController : MonoBehaviour
 {
+    public TextMeshProUGUI text_display;
     public int dirt;
 
     public static ResourceController GetObject()
     {
         return GameObject.Find("ResourceController").GetComponent<ResourceController>();
+    }
+
+    private void Start()
+    {
+        UpdateDisplay();
     }
 
     public bool CanPurchase(int amount)
@@ -22,10 +30,18 @@ public class ResourceController : MonoBehaviour
     public void AddResource(int amount)
     {
         dirt += amount;
+        UpdateDisplay();
     }
     
     public void RemoveResource(int amount)
     {
         dirt -= amount;
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        text_display.text = $"X{dirt}";
+
     }
 }
