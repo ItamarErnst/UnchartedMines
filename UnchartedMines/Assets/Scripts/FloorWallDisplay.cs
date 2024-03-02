f402b686d02c7b041b35d0e5ea3bc6d7dcb55474
Itamar\Erlich <Itamar@lonebot.net>
2024-03-02 10:09:22 +0200

    Floor colors

---
+++ UnchartedMines/Assets/Scripts/FloorWallDisplay.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorWallDisplay : BaseWallDisplay
+{
+    public SpriteRenderer renderer;
+    public List<Color> floor_color_list = new();
+
+    public override void SetDisplay(WallData data)
+    {
+        base.SetDisplay(data);
+        renderer.color = floor_color_list[Random.Range(0, floor_color_list.Count)];
+    }
+}
