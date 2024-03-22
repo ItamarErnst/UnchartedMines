aa4ef5b6e2f9b33963eb173d36edbad9f076cbb6
YuvalVilner <yuvalvilner@gmail.com>
2024-03-22 14:05:24 +0100

    Torch Off

--- UnchartedMines/Assets/Scripts/FloorWallDisplay.cs
+++ UnchartedMines/Assets/Scripts/FloorWallDisplay.cs
@@ -1,21 +1,26 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿    using System.Collections.Generic;
+    using UnityEngine;
 
-public class FloorWallDisplay : BaseWallDisplay
-{
-    public SpriteRenderer renderer;
-    public List<Color> floor_color_list = new();
-    
-    public GameObject torch;
-
-    public override void SetDisplay(WallData data)
+    public class FloorWallDisplay : BaseWallDisplay
     {
-        base.SetDisplay(data);
-        renderer.color = floor_color_list[Random.Range(0, floor_color_list.Count)];
-        torch.SetActive(data.wallType == WallType.Torch);
-        if(data.wallType == WallType.Torch)
-         {
-         FMODUnity.RuntimeManager.PlayOneShot("event:/Sfx/Ligh", GetComponent<Transform>().position);
-         }
+        public SpriteRenderer renderer;
+        public List<Color> floor_color_list = new();
+        public GameObject torch;
+        public override void SetDisplay(WallData data)
+        {
+            base.SetDisplay(data);
+            renderer.color = floor_color_list[Random.Range(0, floor_color_list.Count)];
+            torch.SetActive(data.wallType == WallType.Torch);
+            if(data.wallType == WallType.Torch)
+            {
+            torch.SetActive(true);
+            FMODUnity.RuntimeManager.PlayOneShot("event:/Sfx/Ligh", GetComponent<Transform>().position); 
+            }
+
+
+            else
+            {torch.SetActive(false);
+            FMODUnity.RuntimeManager.PlayOneShot("event:/Sfx/LighOff", GetComponent<Transform>().position);
+            }
+        }
     }
-}
